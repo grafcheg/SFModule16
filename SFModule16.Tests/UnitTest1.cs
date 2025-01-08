a6802a0c23111ef86c524bb322094975e553563c
Vitaly Efremov <dergraff@gmail.com>
2025-01-08 14:02:32 +1000

    Initial commit

--- SFModule16.Tests/UnitTest1.cs
+++ SFModule16.Tests/UnitTest1.cs
@@ -8,9 +8,37 @@ public class Tests
     }
 
     [Test]
+    public void AdditionalTest()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Additional(300, 10), Is.EqualTo(310));
+    }
+    
+    [Test]
     public void SubtractionTest()
     {
         Calculator calculator = new Calculator();
         Assert.That(calculator.Subtraction(300, 10), Is.EqualTo(290));
+    }
+
+    [Test]
+    public void MultiplicationTest()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Miltiplication(3, 100), Is.EqualTo(300));
+    }
+    
+    [Test]
+    public void DivisionTest()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(calculator.Division(300, 100), Is.EqualTo(3));
+    }
+    
+    [Test]
+    public void DivisionByZeroTest()
+    {
+        Calculator calculator = new Calculator();
+        Assert.That(() => calculator.Division(300, 0), Throws.InstanceOf<DivideByZeroException>());
     }
 }
